a3ff3107eebfc46c126c91de574aba3e2f73ac2d
sam1610 <samir.amri@gmail.com>
2023-12-03 21:01:33 +0300

    Change to HOla

--- App.tsx
+++ App.tsx
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.error();
+  (" Hola  ");
+  ("Warning Message ");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hola  a Todos!</Text>
       <StatusBar style="auto" />
     </View>
   );
